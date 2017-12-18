97519b526ce66a35d69d9858b8f35c1925f88881
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-18 08:11:28 -0600

    Updated the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <h1>Home</h1>
 <p>My name is Klaus Donnert. This website is a basic portfolio of some of the things that I can do.
-    There is nothing really fancy here, just some demo sites to showcase some of my work.
+    There is nothing really fancy here, just some demo sites to show that I can work with html, css,
+    js, php, mysql, postgres, laravel and other frameworks and libraries.
     This site is built with Laravel 5.5 on a small LEMP server hosted in the cloud at Digitalocean.
     It uses Laravel Forge to automatically pull updates from Github, run Composer and Artisan. The source code is located at
     <a href="https://github.com/klaus-donnert/basicwebsite">https://github.com/klaus-donnert/basicwebsite</a>.</p>
