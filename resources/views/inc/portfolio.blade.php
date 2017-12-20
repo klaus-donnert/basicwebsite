80f88a7850e3444f1acecd555314b426e7d7c207
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-20 06:25:09 -0600

    adjust rows

--- resources/views/inc/portfolio.blade.php
+++ resources/views/inc/portfolio.blade.php
@@ -1,13 +1,13 @@
 <h3 class="text-center">Portfolio</h3>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-xs-12 col-md-8" title="A simple Codepen using only html, css, and bootstrap 3">
+        <div class="col-xs-3" title="A simple Codepen using only html, css, and bootstrap 3">
             <a target="_blank" href="https://codepen.io/klaus-donnert/full/NjMwWd/">
                 <img class="thumbnail" width="200" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
 
             </a>
         </div>
-        <div class="col-xs-12 col-md-8" title="A simple photo gallery using Laravel 5.5 and mysql">
+        <div class="col-xs-3" title="A simple photo gallery using Laravel 5.5 and mysql">
             <a target="_blank" href="https://photoshow.donnert.us/">
                 <img class="thumbnail" width="200" src="images/photoshow.png" alt="a photo album">
 
