f7778e26a453d4193e687f7a0b6762ef36267f73
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-20 07:02:32 -0600

    add a pic

--- resources/views/inc/portfolio.blade.php
+++ resources/views/inc/portfolio.blade.php
@@ -3,13 +3,13 @@
     <div class="row">
         <div class="col-xs-3" title="A simple Codepen using only html, css, and bootstrap 3">
             <a target="_blank" href="https://codepen.io/klaus-donnert/full/NjMwWd/">
-                <img class="thumbnail" width="200" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
+                <img width="200" height="200" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
 
             </a>
         </div>
         <div class="col-xs-3" title="A simple photo gallery using Laravel 5.5 and mysql">
             <a target="_blank" href="https://photoshow.donnert.us/">
-                <img class="thumbnail" width="200" src="images/photoshow.png" alt="a photo album">
+                <img width="200" height="200" src="images/photoshow.png" alt="a photo album">
 
             </a>
         </div>
