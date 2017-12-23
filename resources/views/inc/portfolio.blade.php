5ca0b953d03cbd60c06ef07b9e5401e661daa7e2
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-23 14:04:13 -0600

    update css

--- resources/views/inc/portfolio.blade.php
+++ resources/views/inc/portfolio.blade.php
@@ -1,14 +1,13 @@
+<style>
+
+</style>
 <h3 class="text-center">Portfolio</h3>
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-4">
-            <div class="card" style="width: 14rem;">
+            <div class="card boxshadow" style="width: 14rem;">
                 <img class="card-img-top" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
                 <div class="card-body">
-                    <br>
-                    <br>
-                    <br>
-                    <br>
                     <h4 class="card-title">A Tribute</h4>
                     <p class="card-text">This Codepen uses only html, css, and bootstrap&nbsp;3
                     </p>
@@ -17,7 +16,7 @@
             </div>
         </div>
         <div class="col-sm-4">
-            <div class="card" style="width: 14rem;">
+            <div class="card boxshadow" style="width: 14rem;">
                 <img class="card-img-top" src="images/photoshow.png"
                      alt="A simple photo gallery using Laravel 5.5 and mysql">
                 <div class="card-body">
