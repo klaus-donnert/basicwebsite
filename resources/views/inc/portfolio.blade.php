bc3da64a5110898e05e30dbfc54756ee0647f52e
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-24 20:57:54 -0600

    add tolist.donnert.us card

--- resources/views/inc/portfolio.blade.php
+++ resources/views/inc/portfolio.blade.php
@@ -6,19 +6,38 @@
     <div class="row">
         <div class="col-sm">
             <div class="card boxshadow" style="width: 14rem;">
-                <img class="card-img-top" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
+                <a href="https://todolist.donnert.us/">
+                    <img class="card-img-top" src="images/todolist.png" alt="A todo list">
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title">Todo List</h4>
+                    <p class="card-text">The obligatory Todo List
+                    </p>
+                    <a href="https://todolist.donnert.us/" class="btn btn-primary">Visit Site</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm">
+            <div class="card boxshadow" style="width: 14rem;">
+                <a href="https://codepen.io/klaus-donnert/full/NjMwWd/">
+                    <img class="card-img-top" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
+                </a>
                 <div class="card-body">
                     <h4 class="card-title">A Tribute</h4>
-                    <p class="card-text">This Codepen uses only html, css, and bootstrap&nbsp;3
+                    <p class="card-text">This Codepen uses only html, css, and bootstrap&nbsp;3. No JavaScript.
                     </p>
                     <a href="https://codepen.io/klaus-donnert/full/NjMwWd/" class="btn btn-primary">Visit Site</a>
                 </div>
             </div>
         </div>
+
         <div class="col-sm">
             <div class="card boxshadow" style="width: 14rem;">
-                <img class="card-img-top" src="images/photoshow.png"
-                     alt="A simple photo gallery using Laravel 5.5 and mysql">
+                <a href="https://photoshow.donnert.us/">
+                    <img class="card-img-top" src="images/photoshow.png"
+                         alt="A simple photo gallery using Laravel 5.5 and mysql">
+                </a>
                 <div class="card-body">
                     <h4 class="card-title">Photo Show</h4>
                     <p class="card-text">A photo album site using Laravel 5.5 and Foundation 6.3
