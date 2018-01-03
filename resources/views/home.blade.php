1622d98255bae305006f77c0e9bf491f98090a46
Klaus Donnert <dieter.donnert@gmail.com>
2018-01-03 06:14:29 -0600

    upddate home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <h1>Home</h1>
-<p>My name is Klaus Donnert. This website is a basic portfolio of some of the things that I can do.
-    There is nothing really fancy here, just some demo sites to show that I can work with html, css,
-    js, php, mysql, postgres, laravel and other frameworks and libraries.
+<p>My name is Klaus Donnert. This website is a example of some of the things that I can do.
+    There is nothing really fancy here, just some demo sites to with html, css,
+    js, vue.js, jquery, php, mysql, sqlite, laravel, and other frameworks and libraries.
     This site is built with Laravel 5.5 on a small LEMP server hosted in the cloud at Digitalocean.
-    It uses Laravel Forge to automatically pull updates from Github, run Composer and Artisan. The source code is located at
+    I'm using Laravel Forge to automatically pull updates from Github, run Composer and Artisan whenever a push to master occurs.
+    The source code is located at
     <a href="https://github.com/klaus-donnert/basicwebsite">https://github.com/klaus-donnert/basicwebsite</a>.</p>
 
     @include('inc.portfolio')
