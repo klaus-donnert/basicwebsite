47dfe589ec3ce16e582e55a355b8861b6b5399d2
Klaus Donnert <dieter.donnert@gmail.com>
2018-01-13 19:30:03 -0600

    fix broken link

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Donnert</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+    <a class="navbar-brand" href="/">Donnert</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDefault"
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <div class="collapse navbar-collapse" id="navbarDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
                 <a class="nav-link" href="/">Home</a>
--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron test-center">
 	<div class="container">
     <h1>Welcome To Donnert.us</h1>
-    <p class=lead>This site was built using Laravel 5.5, Mysql</p>
+    <p class=lead>This responsive website was built using Laravel 5.5 and Mysql</p>
 	</div>
 </div>
