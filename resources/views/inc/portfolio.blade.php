a0dfb6d9df8578a23ef5a491bca3c9350b467a7d
Klaus Donnert <dieter.donnert@gmail.com>
2018-01-23 07:04:23 -0600

    updated home page

--- resources/views/inc/portfolio.blade.php
+++ resources/views/inc/portfolio.blade.php
@@ -1,12 +1,14 @@
 <style>
-
+.pad-bot{
+    padding-bottom: 20px;
+}
 </style>
 <h3 class="text-center">Recent Projects</h3>
 <div class="container">
 
     <div class="row">
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
                 <a href="https://yc.idispatcher.net/map/mapbox.php?avt=true&simm=true">
                     <img class="card-img-top" src="images/gps_map.png"
@@ -14,39 +16,40 @@
                 </a>
                 <div class="card-body">
                     <h4 class="card-title">GPS Map</h4>
-                    <p class="card-text">A map containing the simulated locations of taxicabs. Built using Mapbox and Leaflet
+                    <p class="card-text">A map containing the simulated locations of taxicabs. Built using Mapbox and
+                        Leaflet
                     </p>
-                    <a href="https://yc.idispatcher.net/map/mapbox.php?avt=true&simm=true" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
                 <a href="/idispatcher">
                     <img class="card-img-top" src="images/idispatcher_gps_logo.png"
-                                                  alt="A Placeholder">
+                         alt="A Placeholder">
                 </a>
                 <div class="card-body">
                     <h4 class="card-title">Mobile GPS</h4>
                     <p class="card-text">This is the mobile app that puts the cars on the GPS map.<br><br><br>
                     </p>
-                    <a href="/idispatcher" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
-                <a href="https://placeholder.donnert.us/">
-                    <img class="card-img-top" src="images/placeholder.png"
-                         alt="A Placeholder">
+                <a href="https://twitter.donnert.us/">
+                    <img class="card-img-top" src="images/twitter.png"
+                         alt="An example use of the Twitter API">
                 </a>
                 <div class="card-body">
-                    <h4 class="card-title">Placeholder</h4>
-                    <p class="card-text">Placeholder
+                    <h4 class="card-title">Twitter API</h4>
+                    <p class="card-text">Example of using the Twitter API
                     </p>
-                    <a href="https://placeholder.donnert.us/" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
@@ -56,36 +59,36 @@
     <br>
     <div class="row">
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
                 <a href="https://todolist.donnert.us/">
                     <img class="card-img-top" src="images/todolist.png" alt="A todo list">
                 </a>
                 <div class="card-body">
                     <h4 class="card-title">Todo List</h4>
-                    <p class="card-text">The obligatory Todo List
+                    <p class="card-text">No Ajax
                     </p>
-                    <a href="https://todolist.donnert.us/" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
-                <a href="https://codepen.io/klaus-donnert/full/NjMwWd/">
-                    <img class="card-img-top" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
+                <a href="https://klaus-donnert.github.io/itemapi/">
+                    <img class="card-img-top" src="images/itemapi.png"
+                         alt="A simple API">
                 </a>
                 <div class="card-body">
-                    <h4 class="card-title">A Tribute</h4>
-                    <p class="card-text">This Codepen uses only html, css, and bootstrap&nbsp;3. No JavaScript.
+                    <h4 class="card-title">Todo List</h4>
+                    <p class="card-text">Using Ajax via Axios and a simple API
                     </p>
-                    <a href="https://codepen.io/klaus-donnert/full/NjMwWd/" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
 
-
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
                 <a href="https://photoshow.donnert.us/">
                     <img class="card-img-top" src="images/photoshow.png"
@@ -97,7 +100,7 @@
                         <br>
                         <br>
                     </p>
-                    <a href="https://photoshow.donnert.us/" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
@@ -107,37 +110,21 @@
     <br>
     <div class="row">
 
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
-                <a href="https://klaus-donnert.github.io/itemapi/">
-                    <img class="card-img-top" src="images/itemapi.png"
-                         alt="A simple API">
+                <a href="https://codepen.io/klaus-donnert/full/NjMwWd/">
+                    <img class="card-img-top" src="images/tribute.png" alt="A Codepen tribute to Dr Edward Jenner">
                 </a>
                 <div class="card-body">
-                    <h4 class="card-title">Item API</h4>
-                    <p class="card-text">A simple API that could also be a todo list
+                    <h4 class="card-title">A Tribute</h4>
+                    <p class="card-text">This Codepen uses only html, css, and bootstrap&nbsp;3. No JavaScript.
                     </p>
-                    <a href="https://klaus-donnert.github.io/itemapi/" class="btn btn-primary">Visit Site</a>
+
                 </div>
             </div>
         </div>
 
-        <div class="col-sm">
-            <div class="card boxshadow" style="width: 14rem;">
-                <a href="https://twitter.donnert.us/">
-                    <img class="card-img-top" src="images/twitter.png"
-                         alt="An example use of the Twitter API">
-                </a>
-                <div class="card-body">
-                    <h4 class="card-title">Twitter API</h4>
-                    <p class="card-text">Example of using the Twitter API
-                    </p>
-                    <a href="https://twitter.donnert.us/" class="btn btn-primary">Visit Site</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm">
+        <div class="col-sm pad-bot">
             <div class="card boxshadow" style="width: 14rem;">
                 <a href="https://placeholder.donnert.us/">
                     <img class="card-img-top" src="images/placeholder.png"
@@ -147,7 +134,22 @@
                     <h4 class="card-title">Placeholder</h4>
                     <p class="card-text">Placeholder
                     </p>
-                    <a href="https://placeholder.donnert.us/" class="btn btn-primary">Visit Site</a>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm pad-bot">
+            <div class="card boxshadow" style="width: 14rem;">
+                <a href="https://placeholder.donnert.us/">
+                    <img class="card-img-top" src="images/placeholder.png"
+                         alt="A Placeholder">
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title">Placeholder</h4>
+                    <p class="card-text">Placeholder
+                    </p>
+
                 </div>
             </div>
         </div>
